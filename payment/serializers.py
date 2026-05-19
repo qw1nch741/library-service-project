de552cf37fa8209542150d88de619ccfd64fafcf
Kyryl <sautakiki.com@gmail.com>
2026-05-19 11:14:40 +0300

    added payment/serializers.py

---
+++ payment/serializers.py
@@ -0,0 +1,22 @@
+from rest_framework import serializers
+
+from payment.models import Payment
+
+
+class PaymentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Payment
+        fields = (
+            "id",
+            "status",
+            "type",
+            "borrowing",
+            "session_url",
+            "session_id",
+            "money_to_pay"
+        )
+        extra_kwargs = {
+            "session_url": {"read_only": True},
+            "session_id": {"read_only": True},
+            "money_to_pay": {"read_only": True}
+        }
