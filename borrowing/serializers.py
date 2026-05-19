f0f7a7104c108eab7a8473cbb783764488bcd514
Kyryl <sautakiki.com@gmail.com>
2026-05-19 11:14:07 +0300

    added serializers for borrowing

---
+++ borrowing/serializers.py
@@ -0,0 +1,23 @@
+from rest_framework import serializers
+from borrowings.models import Borrowing
+from books.serializers import BookSerializer
+
+
+class BorrowingReadSerializer(serializers.ModelSerilizer):
+    book = BookSerializer(read_only=True)
+
+    class Meta:
+        model = Borrowing
+        fields = (
+            "id",
+            "borrow_date",
+            "expected_return_date",
+            "actual_return_date",
+            "book",
+            "user"
+        )
+
+class BorrowingCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Borrowing
+        fields = ("id", "book", "expected_return_date")
