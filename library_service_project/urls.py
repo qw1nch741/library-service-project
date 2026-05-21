b18a3f6966dd0214791dd736be7b80a60c9a9336
Kyryl <sautakiki.com@gmail.com>
2026-05-21 11:53:09 +0300

    added swagger to urls

--- library_service_project/urls.py
+++ library_service_project/urls.py
@@ -2,14 +2,17 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
-# 1. Define the main list of paths
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/books/', include('books.urls', namespace='books')),
     path('api/borrowings/', include('borrowings.urls', namespace='borrowings')),
     path('api/users/', include('users.urls', namespace='users')),
     path('api-auth/', include('rest_framework.urls')),
+    path("api/schema/", SpectacularAPIView.as_view(), name='schema'),
+    path("api/schema/swagger-ui/", SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
 ]
 
 if settings.DEBUG:
